abfa5c631cb611afa89735bdf79b8528892e6dd1
kostiantyn <kostiantyn@test.com>
2024-05-19 15:10:48 -0230

    fix: syntax mistake

--- java/spring/laurentiu-spilca-spring-starts-here/rest-clients/rest-client/src/main/java/org/practice/config/AspectConfig.java
+++ java/spring/laurentiu-spilca-spring-starts-here/rest-clients/rest-client/src/main/java/org/practice/config/AspectConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
-//EnableAspectJAutoProxy
+//@EnableAspectJAutoProxy
 public class AspectConfig {
     @Bean
     public LoggerAspect loggerAspect() {
