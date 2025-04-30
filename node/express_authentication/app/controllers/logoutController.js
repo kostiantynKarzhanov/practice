7b4e33ae46b5590f4ebda730573821bf6f7f88ca
kostiantynKarzhanov <102972154+kostiantynKarzhanov@users.noreply.github.com>
2025-04-30 18:27:15 -0230

    modified: logout functionality for session based auth

--- node/express_authentication/app/controllers/logoutController.js
+++ node/express_authentication/app/controllers/logoutController.js
@@ -1,7 +1,15 @@
-const handleLogout = (req, res) => {
-    // You cannot directly force the browser to delete a cookie from a server response — but you can instruct it to expire the cookie immediately.
-    // clearCookie sets a cookie with a given name to expiration date in the past: 01 Jan 1970 00:00:00 GMT
-    res.clearCookie('auth', { httpOnly: true });
+// ----- import services -----
+import { deleteSession } from '../services/sessionService.js';
+
+const handleLogout = async (req, res) => {
+    const { sid } = req.cookies;
+
+    if (sid) {
+        await deleteSession(sid);
+        // You cannot directly force the browser to delete a cookie from a server response — but you can instruct it to expire the cookie immediately.
+        // clearCookie sets a cookie with a given name to expiration date in the past: 01 Jan 1970 00:00:00 GMT
+        res.clearCookie('sid', { httpOnly: true, secure: true });
+    }
 
     return res.redirect('/login');
 };
--- node/express_authentication/app/services/sessionService.js
+++ node/express_authentication/app/services/sessionService.js
@@ -3,10 +3,10 @@ import SessionModel from '../models/SessionModel.js';
 
 const createSession = (sid, username) => {
     const maxAge = 1000 * 60 * 60; // 1 hour
-    const expires = Date.now() + maxAge; 
+    const expires = Date.now() + maxAge;
 
     return SessionModel.create({ sid, username, expires });
-    
+
     // these code works the same as .create():
     // const session = new SessionModel({ sid, username, expires });
     // return session.save();
@@ -14,10 +14,13 @@ const createSession = (sid, username) => {
 
 const findSession = (sid) => SessionModel.findOne({ sid }).exec();
 
+const deleteSession = (sid) => SessionModel.deleteOne({ sid }).exec();
+
 const verifySession = (session) => session && session.expires > Date.now();
 
 export {
     createSession,
     findSession,
-    verifySession
-}
+    deleteSession,
+    verifySession,
+};
