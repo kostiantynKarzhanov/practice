adddf5fdbc1d5da8a714cfc24c719da6ca32ead5
kostiantynKarzhanov <102972154+kostiantynKarzhanov@users.noreply.github.com>
2025-05-01 17:51:35 -0230

    modified: moved cookie name to .env file

--- node/express_authentication/app/controllers/logoutController.js
+++ node/express_authentication/app/controllers/logoutController.js
@@ -6,9 +6,10 @@ const handleLogout = async (req, res) => {
 
     if (sid) {
         await deleteSession(sid);
+        
         // You cannot directly force the browser to delete a cookie from a server response — but you can instruct it to expire the cookie immediately.
         // clearCookie sets a cookie with a given name to expiration date in the past: 01 Jan 1970 00:00:00 GMT
-        res.clearCookie('sid', { httpOnly: true, secure: true });
+        res.clearCookie(process.env.SESSION_COOKIE_NAME, { httpOnly: true, secure: true });
     }
 
     return res.redirect('/login');
