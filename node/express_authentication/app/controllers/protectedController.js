1e7fc33555a142106cac7bb4bcde8eec985b8a94
kostiantynKarzhanov <102972154+kostiantynKarzhanov@users.noreply.github.com>
2025-05-07 11:39:43 -0230

    new: added sessionOriginalMaxAgeSec, sessionMaxAgeBeforeReloadSec
    modified: get user from object stored in req.session

--- node/express_authentication/app/controllers/protectedController.js
+++ node/express_authentication/app/controllers/protectedController.js
@@ -1,7 +1,9 @@
 const handleProtectedView = (req, res) => {
-    const { username } = req.user;
+    const username = req.session.user;
+    const sessionOriginalMaxAgeSec = req.session.cookie.originalMaxAge / 1000;
+    const sessionMaxAgeBeforeReloadSec = req.session.cookie.maxAge / 1000;
 
-    return res.render('protected', { h1: 'Protected', username  });
+    return res.render('protected', { h1: 'Protected', username, sessionOriginalMaxAgeSec, sessionMaxAgeBeforeReloadSec });
 };
 
 export {
