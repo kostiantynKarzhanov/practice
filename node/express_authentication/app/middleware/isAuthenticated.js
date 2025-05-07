d90f29d6e4a221fd3dd4c9448e5f8af1e88d444b
kostiantynKarzhanov <102972154+kostiantynKarzhanov@users.noreply.github.com>
2025-05-07 11:37:13 -0230

    modified: check if user object exists on the current session

--- node/express_authentication/app/middleware/isAuthenticated.js
+++ node/express_authentication/app/middleware/isAuthenticated.js
@@ -1,24 +1,6 @@
-// ----- import services -----
-import { verifyUser } from "../services/userService.js";
-
 const isAuthenticated = async (req, res, next) => {
     try {
-        const { auth } = req.cookies;
-
-        if (auth && auth.startsWith('Basic')) {
-            const credentialsBase64 = auth.split(' ')[1];
-            const credentials = Buffer.from(credentialsBase64, 'base64').toString('utf8');
-            const [username, password] = credentials.split(':');
-            const isVerified = await verifyUser(username, password);
-
-            if (isVerified) {
-                req.user = {
-                    username
-                };
-                
-                return next();
-            }
-        }
+        if (req.session.user) return next();
 
         return res.status(401).json({
             status: 'error',
