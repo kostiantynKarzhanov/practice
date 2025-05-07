0a4e342b8e3fbada7d38c79c0c93ec5f4a5f6c5d
kostiantynKarzhanov <102972154+kostiantynKarzhanov@users.noreply.github.com>
2025-05-07 10:52:43 -0230

    modified: handleLogin to use express-session functionality

--- node/express_authentication/app/controllers/loginController.js
+++ node/express_authentication/app/controllers/loginController.js
@@ -1,5 +1,5 @@
 // ----- import services -----
-import { loginUser } from '../services/userService.js';
+import { verifyUser } from '../services/userService.js';
 
 const renderLoginView = (req, res) => {
     return res.render('login', { h1: 'Login', action: 'login' });
@@ -8,31 +8,42 @@ const renderLoginView = (req, res) => {
 const handleLogin = async (req, res, next) => {
     try {
         const { username, password } = req.body;
-        const authCookieValue = await loginUser(username, password);
-        
-        if (authCookieValue) {
-            const authCookieOptions = {
-                httpOnly: true,
-                maxAge: (1000 * 60 * 60) // 1 hour
-            };
-    
-            res.cookie('auth', authCookieValue, authCookieOptions);
-            return res.redirect(303, '/protected');
-        } else {
-            return res.status(401).json({ 
-                status: 'error', 
-                message: 'Invalid credentials.' 
+        const isVerified = await verifyUser(username, password);
+
+        if (isVerified) {
+            // regenerate the session, which is good practice to help guard against forms of session fixation
+            // a new SID and Session instance will be initialized at req.session
+            return req.session.regenerate((err) => {
+                if (err) return next(err);
+
+                console.log('session regenerated:', req.session.id);
+                req.session.user = username;
+
+                // Save the session back to the store, replacing the contents on the store with the contents in memory. This method is automatically called at the end of the HTTP response if the session data has been altered. Because of this, typically this method does not need to be called. There are some cases where it is useful to call this method, for example, redirects, long-lived requests or in WebSockets.
+                
+                // save the session before redirection to ensure page load does not happen before session is saved
+                return req.session.save((err) => {
+                    if (err) return next(err);
+
+                    console.log('session saved:', req.session.id);
+                    return res.redirect(303, '/protected');
+                });
             });
         }
+
+        return res.status(401).json({
+            status: 'error',
+            message: 'Invalid credentials.'
+        });
     } catch (err) {
         console.error(err.stack);
-            
+
         return next(err);
     }
 };
 
-export { 
-    renderLoginView, 
-    handleLogin 
+export {
+    renderLoginView,
+    handleLogin
 };
 
