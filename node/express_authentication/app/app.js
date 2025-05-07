e58ef3686ccda9ffb12625bb1bd3453b65a4ae84
kostiantynKarzhanov <102972154+kostiantynKarzhanov@users.noreply.github.com>
2025-05-07 10:47:04 -0230

    removed: sessionConfig
    moved: session configuration to app.js
    
    with resave and rolling set to true session expiration date gets reset on each server's response (Set cookie send with new date)

--- node/express_authentication/app/app.js
+++ node/express_authentication/app/app.js
@@ -5,10 +5,10 @@ import { fileURLToPath } from 'node:url';
 import { dirname, join } from 'node:path';
 import mongoose from 'mongoose';
 import session from 'express-session';
+import MongoStore from 'connect-mongo';
 
 // ----- import custom modules -----
 import connectDatabase from './config/connectDatabase.js';
-import sessionConfig from './config/sessionConfig.js';
 import { stopServer } from './utils/serverUtils.js';
 
 // ----- import routers -----
@@ -36,7 +36,31 @@ app.set('x-powered-by', false);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(session(sessionConfig));
+app.use(session({
+    name: process.env.SESSION_COOKIE_NAME,
+    store: MongoStore.create({ client: mongoose.connection.client }),
+    secret: process.env.SESSION_SECRET.split(','),
+    // Forces the session to be saved back to the session store, even if the session was never modified during the request.
+    resave: true,
+    // Forces a session that is "uninitialized" to be saved to the store. A session is uninitialized when it is new but not modified. Choosing false is useful for implementing login sessions, reducing server storage usage
+    // Note if you are using Session in conjunction with PassportJS, Passport will add an empty Passport object to the session for use after a user is authenticated, which will be treated as a modification to the session, causing it to be saved. 
+    saveUninitialized: false,
+    // Force the session identifier cookie to be set on every response. The expiration is reset to the original maxAge, resetting the expiration countdown.
+    // With this enabled, the session identifier cookie will expire in maxAge since the last response was sent instead of in maxAge since the session was last modified by the server.
+    rolling: true,
+    cookie: {
+        // If secure is true, and you access your site over HTTP, the cookie will not be set 
+        // secure: true,
+        maxAge: 1000 * 60 // 1 min
+    }
+}));
+
+app.use((req, res, next) => {
+    console.log('current session id:', req.session.id);
+    console.log('current session expires in (s):', req.session.cookie.maxAge / 1000);
+
+    next();
+});
 
 // ----- define app routes -----
 app.use('/', indexRouter);
