5fa11dd0cdff5c7345e864950fd4be3dfe991296
kostiantynKarzhanov <102972154+kostiantynKarzhanov@users.noreply.github.com>
2025-05-12 19:38:32 -0230

    modified: added res.clearCookie to instruct browser to expire cookie after logout

--- node/express_authentication/app/controllers/logoutController.js
+++ node/express_authentication/app/controllers/logoutController.js
@@ -1,7 +1,9 @@
 const handleLogout = (req, res) => {
     // You cannot directly force the browser to delete a cookie from a server response — but you can instruct it to expire the cookie immediately.
     // clearCookie sets a cookie with a given name to expiration date in the past: 01 Jan 1970 00:00:00 GMT
-    res.clearCookie('auth', { httpOnly: true });
+    // res.clearCookie('auth', { httpOnly: true });
+
+    res.clearCookie(process.env.JWT_COOKIE_NAME, { sameSite: 'strict', httpOnly: true, secure: true });
 
     return res.redirect(303, '/login');
 };
